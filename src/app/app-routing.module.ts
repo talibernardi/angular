ad7920e79264f258c61a44a5f06f5ad1ac6d3467
Talita Bernardino Neves Silva <talita.silva@db1.com.br>
2021-12-13 14:18:53 -0300

    versionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,34 @@
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import {ThirdComponent } from './third/third.component';
+
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { NotFoundError } from 'rxjs';
 
 const routes: Routes = [
   { path: 'first-component', component: FirstComponent },
   { path: 'second-component', component: SecondComponent },
+  { path: 'third-component', component: ThirdComponent },
+  { path: '**', component: NotFoundError },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.route = params['route'];
+    });
+  }
+}
